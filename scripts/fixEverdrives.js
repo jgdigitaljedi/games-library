e3fcec0cb6575e0f18b03cb743ccdb64a6d3630f
Joey Gauthier <digitaljedi@outlook.com>
2020-03-16 07:05:07 -0500

    fixed everdrives list, added new games

---
+++ scripts/fixEverdrives.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+const ed = require('../server/extra/everDrive.json');
+
+const fixed = ed.map(d => {
+  d.consoleArr = [{ consoleName: d.consoleName, consoleId: d.consoleIgdbId }];
+  return d;
+});
+
+fs.writeFile(
+  path.join(__dirname, '../server/extra/everDrive.json'),
+  JSON.stringify(fixed, null, 2),
+  error => {
+    if (error) {
+      console.log(chalk.red.bold(error));
+    } else {
+      console.log(chalk.green('EverDrive entries have added consoleArr!'));
+    }
+  }
+);

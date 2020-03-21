6b0c088b1a3f77684cac99f0da3394764dfda86f
Joey Gauthier <digitaljedi@outlook.com>
2020-03-21 07:31:40 -0500

    wrote bash script to copy games from other project when on personal laptop

---
+++ scripts/copyGamesFromOtherProject.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# only run this from my main laptop since it runs Windows 10 (barf) with WSL
+
+cp /mnt/c/code/homeControl/server/db/games.json server/db/games.json
+
+npm run process

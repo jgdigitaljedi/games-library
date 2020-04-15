65c9fdda2c0f78c55ece71d7694221bb01410cc4
Joey Gauthier <jgauthier@indeed.com>
2020-04-15 08:43:14 -0500

    added routing to urlService

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -2,5 +2,6 @@ const piServer = process.env.REACT_APP_PISERVER;
 
 export default {
   prefix: piServer ? 'http://ghome.help:4001' : 'http://localhost:4001',
-  assets: piServer ? 'http://ghome.help/gameslib/' : ''
+  assets: piServer ? 'http://ghome.help/gameslib/' : '',
+  routing: piServer ? 'gameslib/' : ''
 };

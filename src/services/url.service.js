1507e2017b78c0e5ddd4a9986a51b2b7e5ab688d
Joey Gauthier <digitaljedi@outlook.com>
2020-04-15 06:51:19 -0500

    added games, got it to build in a way that my pi server will run it properly

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -2,5 +2,5 @@ const piServer = process.env.REACT_APP_PISERVER;
 console.log('piserver', piServer);
 
 export default {
-  prefix: piServer ? 'http://192.168.0.152:4001' : 'http://localhost:4001'
+  prefix: piServer ? 'http://ghome.help:4001' : 'http://localhost:4001'
 };

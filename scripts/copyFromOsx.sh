5df1111220f870fcdf5728f849b5b302d35b5add
Joey Gauthier <jgauthier@indeed.com>
2020-06-16 08:26:45 -0500

    updated library, added copy script for mac that should work for Linux too

---
+++ scripts/copyFromOsx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# only run this from my work laptop and Mac Mini
+
+cp ~/code/homeControl/server/db/games.json server/db/games.json
+
+npm run process
--- scripts/copyGamesFromOtherProject.sh
+++ scripts/copyGamesFromOtherProject.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# only run this from my main laptop since it runs Windows 10 (barf) with WSL
+# only run this from my main laptop and gaming PC since they run Windows 10 (barf) with WSL
 
 cp /mnt/c/code/homeControl/server/db/games.json server/db/games.json
 

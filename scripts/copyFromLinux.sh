55b04facbbe2fbe260cdb6a9d58398cf1279518e
Joey Gauthier <digitaljedi@outlook.com>
2020-07-25 07:52:43 -0500

    updated with new Game Over pickups

--- scripts/copyFromLinux.sh
+++ scripts/copyFromLinux.sh
@@ -3,5 +3,6 @@
 # only run this from my work laptop and Mac Mini
 
 cp ~/code/homeControl/server/db/games.json server/db/games.json
+cp ~/code/homeControl/server/db/consoles.json server/db/consoles.json
 
 npm run process

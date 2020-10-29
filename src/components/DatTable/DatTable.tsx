21b61ccd8a6e7de6e007274fbb8a8621fef14e45
Joey Gauthier <digitaljedi@outlook.com>
2020-10-29 07:09:19 -0500

    got most of datatable into functioncompoenent, still has onload issue

--- src/components/DatTable/DatTable.tsx
+++ src/components/DatTable/DatTable.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { FunctionComponent, useCallback, useEffect, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { connect } from 'react-redux';
@@ -15,46 +15,29 @@ interface IProps {
 interface MapStateProps {
   viewWhat: string;
 }
+const TheTable: FunctionComponent<IProps> = ({ data, viewWhat, rowClicked }) => {
+  const [dynamicColumns, setDynamicColumns] = useState<any>();
+  const [selected, setSelected] = useState<any>();
 
-interface ICols {
-  field: string;
-  header: string;
-  sortable?: boolean;
-}
-
-interface IState {
-  cols: ICols[];
-  viewWhat: string;
-  selected: any;
-}
-
-class TheTable extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      cols: [{ field: '', header: '' }],
-      viewWhat: props.viewWhat || '',
-      selected: null
-    };
-  }
-
-  updateDimensions = () => {
+  const updateDimensions = useCallback(() => {
     const tableEle = document.getElementsByClassName('p-datatable-scrollable-body-table');
     const raw = Array.from(tableEle)[0];
-    if (window.innerWidth <= 640) {
+    // @ts-ignore
+    if (window.innerWidth <= 640 && raw?.style) {
       // @ts-ignore
       raw.style.width = '100%';
       setTimeout(() => {
         // @ts-ignore
         raw.style.width = 'auto';
-      }, 50);
-    } else {
+      });
+      // @ts-ignore
+    } else if (raw?.style) {
       // @ts-ignore
       raw.style.width = '100%';
     }
-  };
+  }, []);
 
-  tableItemBody = (rowData: any, info: any) => {
+  const tableItemBody = (rowData: any, info: any) => {
     return (
       <React.Fragment>
         <span className="p-column-title mobile-header">{info.header}</span>
@@ -63,72 +46,12 @@ class TheTable extends Component<IProps, IState> {
     );
   };
 
-  public componentDidMount() {
-    window.addEventListener('resize', this.updateDimensions);
-    this._getCols();
-  }
+  const rowSelected = (row: any) => {
+    setSelected(row);
+    rowClicked && rowClicked(row);
+  };
 
-  public componentWillUnmount() {
-    window.removeEventListener('resize', this.updateDimensions);
-  }
-
-  public componentDidUpdate() {
-    if (this.state.viewWhat !== this.props.viewWhat) {
-      this._getCols();
-      this.setState({ viewWhat: this.props.viewWhat || '' });
-    }
-    this.updateDimensions();
-  }
-
-  public render() {
-    const colProps = this.state.cols || [{ field: '', header: '' }];
-
-    let dynamicColumns = colProps.map((col, i) => {
-      if (col.header === 'Image') {
-        return (
-          <Column
-            key={col.field}
-            header={col.header}
-            body={this._imageTemplate}
-            field={col.field}
-          />
-        );
-      }
-      return (
-        <Column
-          key={col.field}
-          field={col.field}
-          header={col.header}
-          sortable={col.sortable}
-          body={this.tableItemBody}
-        />
-      );
-    });
-    return (
-      <div className="data-table-container">
-        <DataTable
-          value={this.props.data}
-          paginator={true}
-          rows={20}
-          pageLinkSize={10}
-          scrollable={true}
-          selectionMode="single"
-          selection={this.state.selected}
-          onSelectionChange={(e) => this.rowSelected(e.value)}
-          className="p-datatable-gridlines"
-        >
-          {dynamicColumns}
-        </DataTable>
-      </div>
-    );
-  }
-
-  private rowSelected(row: any) {
-    this.setState({ selected: row });
-    this.props.rowClicked && this.props.rowClicked(row);
-  }
-
-  private _imageTemplate(rowData: { gb: { image: string } }): JSX.Element {
+  const _imageTemplate = (rowData: { gb: { image: string } }): JSX.Element => {
     return (
       <img
         src={
@@ -137,7 +60,6 @@ class TheTable extends Component<IProps, IState> {
           'http://localhost:3000/Video-Game-Controller-Icon.svg.png'
         }
         alt="Game cover"
-        // style={imageStyle}
         className="table-image"
         onError={(e: any) => {
           e.target.onerror = null;
@@ -145,12 +67,58 @@ class TheTable extends Component<IProps, IState> {
         }}
       />
     );
-  }
+  };
 
-  private _getCols() {
-    this.setState({ cols: tableProps(this.props.viewWhat || '') });
-  }
-}
+  useEffect(() => {
+    if (viewWhat) {
+      const colProps = tableProps(viewWhat);
+      setDynamicColumns(
+        colProps.map((col, i) => {
+          if (col.header === 'Image') {
+            return (
+              <Column key={col.field} header={col.header} body={_imageTemplate} field={col.field} />
+            );
+          }
+          return (
+            <Column
+              key={col.field}
+              field={col.field}
+              header={col.header}
+              sortable={col.sortable}
+              body={tableItemBody}
+            />
+          );
+        })
+      );
+      updateDimensions();
+    }
+
+    window.addEventListener('resize', updateDimensions);
+
+    return function cleanup() {
+      window.removeEventListener('resize', updateDimensions);
+    };
+    // eslint-disable-next-line
+  }, [viewWhat]);
+
+  return (
+    <div className="data-table-container">
+      <DataTable
+        value={data}
+        paginator={true}
+        rows={20}
+        pageLinkSize={10}
+        scrollable={true}
+        selectionMode="single"
+        selection={selected}
+        onSelectionChange={(e) => rowSelected(e.value)}
+        className="p-datatable-gridlines"
+      >
+        {dynamicColumns}
+      </DataTable>
+    </div>
+  );
+};
 
 const mapStateToProps = ({ viewWhat }: { viewWhat: string }): MapStateProps => {
   return {
